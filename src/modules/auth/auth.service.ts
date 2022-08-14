ac6f6dd3046b14fcc058f6dcccb96261405fd3b1
David Misael Villegas Sandoval <david.misa97@gmail.com>
2022-08-14 02:51:41 -0400

    [Auth]
    -> Update main.ts adding connections to other microservices.
    -> Create module, controller, and services to manage authentications with passportjs

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable, Logger, RequestTimeoutException }          from '@nestjs/common';
+import { catchError, lastValueFrom, throwError, timeout, TimeoutError } from 'rxjs';
+import { compareSync }                                                  from 'bcrypt';
+import { ClientProxy }                                                  from '@nestjs/microservices';
+import { JwtService }                                                   from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject('USER_CLIENT') private readonly client: ClientProxy,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    try {
+      const user = await lastValueFrom(this.client.send({role: 'user', cmd: 'get'}, {username})
+        .pipe(timeout(5000), catchError(err => {
+          if (err instanceof TimeoutError) return throwError(() => new RequestTimeoutException());
+          return throwError(() => err);
+        }),));
+
+      if (compareSync(password, user?.password)) return user;
+
+      return null;
+    } catch (e) {
+      Logger.log(e);
+      throw e;
+    }
+  }
+
+  async login(user) {
+    const payload = {user, sub: user.id};
+
+    return {
+      userId: user.id,
+      accessToken: this.jwtService.sign(payload)
+    };
+  }
+}
